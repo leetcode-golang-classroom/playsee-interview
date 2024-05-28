5e7fb73686360822e22d65af2f4676a9b742db2a
gson Liang <yuanyu90221@gmail.com>
2024-05-28 17:12:13 +0800

    feat(add test failed example): add test failed example

--- handler_test.go
+++ handler_test.go
@@ -12,7 +12,7 @@ import (
 	main "playsee.co/interview"
 )
 
-func Test_Handler(t *testing.T) {
+func Test_SuccessRequest(t *testing.T) {
 	const expectResult = `head: aa
 node1: bb
 node2: cc
@@ -39,5 +39,42 @@ tail: dd
 	json.NewDecoder(rr.Body).Decode(&result)
 	if strings.Compare(result.ToString(), expectResult) != 0 {
 		t.Errorf("result %v, expect %v", result.ToString(), expectResult)
+	}
+}
+
+func Test_FailedWithoutApiKeyRequest(t *testing.T) {
+	const contentType = "application/json"
+	req, err := http.NewRequest("POST", "/api-test", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", contentType)
+	rr := httptest.NewRecorder()
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api-test", main.RequestMiddleware(main.Test1))
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler return wrong status code: got %v want %v", status, http.StatusUnauthorized)
+	}
+}
+
+func Test_FailedWithInvalidPayload(t *testing.T) {
+	const contentType = "application/json"
+	APIKey := "qwerklj1230dsa350123l2k1j4kl1j24"
+	data := []byte(`["aa", "bb", "cc", "dd"]`)
+	req, err := http.NewRequest("POST", "/api-test", bytes.NewBuffer(data))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", contentType)
+	req.Header.Add("api-key", APIKey)
+	rr := httptest.NewRecorder()
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api-test", main.RequestMiddleware(main.Test1))
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler return wrong status code: got %v want %v", status, http.StatusBadRequest)
 	}
 }
